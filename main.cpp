0cc5b92a502d5f4b4544761d6db765bfdf2f5809
Alan <alan.wang@theia.tw>
2024-12-05 16:40:19 +0800

    Add qml module to CMakeLists and integrate main qml to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <QApplication>
+#include <QQmlApplicationEngine>
 
 #include "main_window.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    const QUrl url(QStringLiteral("qrc:/QmlTest/main.qml"));
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    engine.load(url);
 
     ListComPorts();
 
---
+++ main.qml
@@ -0,0 +1,20 @@
+import QtQuick
+import QtQuick.Layouts
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
+
+    Rectangle {
+        width: 200
+        height: 100
+        color: "red"
+
+        Text {
+            anchors.centerIn: parent
+            text: "Hello, World!"
+        }
+    }
+}
