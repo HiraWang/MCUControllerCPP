63c90ab58d6bbea8c0a51153961484fc13dac168
Alan <alan.wang@theia.tw>
2024-12-12 13:39:34 +0800

    Add info comment

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@
 #include "main_window.h"
 
 int main(int argc, char* argv[]) {
+    // Construct Qt Application
     QApplication app(argc, argv);
     QFont font("Arial");
     app.setFont(font);
 
     ListComPorts();
+    
+    // Optimize console
     ResizeConsole(1520, 680);
     DWORD prev_mode;
     HANDLE handle = GetStdHandle(STD_INPUT_HANDLE);
     GetConsoleMode(handle, &prev_mode);
     SetConsoleMode(handle, ENABLE_EXTENDED_FLAGS | (prev_mode & ~ENABLE_QUICK_EDIT_MODE));
 
+    // Construct Qml engine and splash screen
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/QmlSource/splash_screen.qml"));
     QQmlComponent component(&engine, url);
